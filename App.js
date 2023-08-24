d4b02ecdf438a1f59975a9066e08cac64428895f
ErickWillyan <erickwillyan2105@gmail.com>
2023-08-24 18:45:30 -0300

    Criando App js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Erick Editando</Text>
+      <Text>Oi guilherme tudo bem?</Text>
       <StatusBar style="auto" />
     </View>
   );
