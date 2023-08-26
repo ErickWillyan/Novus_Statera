37614c3e4df4402caf452e7355c13863b92762da
ErickWillyan <erickwillyan2105@gmail.com>
2023-08-26 16:32:30 -0300

    Criando Pasta Routes e o arquivo tabNavigator.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Oi Erick</Text>
+      <Text>Oi Erick, testando branch</Text>
       <StatusBar style="auto" />
     </View>
   );
