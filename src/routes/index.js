7413284bd1d34368ffa94142a8b29764966868cd
ErickWillyan <141357058+ErickWillyan@users.noreply.github.com>
2023-10-05 12:27:29 -0300

    editando login

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,13 +5,16 @@ import AuthRoutes from "./auth.routes";
 import AppRoutes from "./main_routes.routes";
 
 export default function Routes() {
-  const singed = true;
-  const loading = true;
+  const singed = false;
+  const loading = false;
 
   if (loading) {
-    <View>
-      <ActivityIndicator color="#fff" />
-    </View>;
+    return(
+    <View style={{ flex:1, justifyContent:"center", alignItems:"center"}}>
+      <ActivityIndicator size={50} color="#008100" />
+    </View>
+
+    )
   }
 
   return singed ? <AppRoutes /> : <AuthRoutes />;
