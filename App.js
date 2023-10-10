952bb7134eb004624e6dab3dd9b2dedc06d90861
ErickWillyan <141357058+ErickWillyan@users.noreply.github.com>
2023-10-10 14:04:30 -0300

    testando login

--- App.js
+++ App.js
@@ -3,15 +3,19 @@ import { NavigationContainer } from "@react-navigation/native";
 import Routes from "./src/routes";
 import { StatusBar } from "react-native";
 
+import AuthProvider from "./src/contexts/auth";
+
 export default function App() {
   return (
     <NavigationContainer>
-      <StatusBar
-        backgroundColor="#36393f"
-        barStyle="light-content"
-        translucent={false}
-      />
-      <Routes />
+      <AuthProvider>
+        <StatusBar
+          backgroundColor="#36393f"
+          barStyle="light-content"
+          translucent={false}
+        />
+        <Routes />
+      </AuthProvider>
     </NavigationContainer>
   );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, ActivityIndicator } from "react-native";
 
 import AuthRoutes from "./auth.routes";
 import AppRoutes from "./main_routes.routes";
 
+import { AuthContext } from "../contexts/auth";
+
 export default function Routes() {
-  const singed = true;
+  const { singed } = useContext(AuthContext);
   const loading = true;
 
   if (loading) {
