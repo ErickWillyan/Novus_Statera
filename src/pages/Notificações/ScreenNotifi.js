f6ca8fe6dd0e7734187f1c43f7e083e98c78a767
EzequielAnjos <92725638+EzequielAnjos@users.noreply.github.com>
2023-11-30 16:39:01 -0300

    adição de props

--- src/pages/Notificações/ScreenNotifi.js
+++ src/pages/Notificações/ScreenNotifi.js
@@ -4,19 +4,21 @@ import {View, Text, StyleSheet, FlatList} from 'react-native';
 export default function ScreenNotifi() {
 
     const [feed, setFeed] = useState([
-        {id: '1', nome: 'Matheus', idade: '50', email: 'matheus@matheus.com'},
-        {id: '2', nome: 'LambaLamba', idade: '30', email: 'lambarina@lubas.com'},
-        {id: '3', nome: 'Maicon', idade: '5', email: 'MJ@jackson.com'},
-        {id: '4', nome: 'Elvis Presley', idade: '80', email: 'elvis@presley.com'},
-
+        {id: '1', nome: 'Mário da Silva de Jesus', cidade: 'Santana de Parnaíba - SP', data: '09 de dezembro, às 13h30'},
+        {id: '2', nome: 'Rafael Martis Araújo', cidade: 'Santana de Parnaíba - SP', data: '13 de dezembro, às 15h10'},
+        {id: '3', nome: 'Renan Costa Pereira', cidade: 'Barueri - SP', data: '10 de dezembro, às 14h00'},
+        {id: '4', nome: 'Michael Jackson', cidade: 'Cajamar - SP', data: '8 de dezembro, às 16h30'},
+        {id: '5', nome: 'Erick Willyan dos Santos Cruz', cidade: 'Santana de Parnaíba - SP', data: '02 de janeiro, às 12h30'},
     ]);
 
     return(
      <View style={styles.container}>
 
         <FlatList
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item) => item.id} 
         data={feed}
-        renderItem={({ item }) => }
+        renderItem={({ item }) => <Pessoa data={item}/>}
         />
 
      </View>
@@ -30,13 +32,20 @@ const styles = StyleSheet.create({
     areaPessoa: {
         backgroundColor: '#121212',
         height: 200,
+        marginBottom: 15,
+    },
+    textoPessoa: {
+        color: '#FFF',
+        fontSize: 20, 
     },
 })
 
-function Pessoa(){
+function Pessoa(props){
     return(
         <View style={styles.areaPessoa}>
-            <Text>Minha Lista</Text>
+            <Text style={styles.textoPessoa}>{props.data.nome}</Text>
+            <Text style={styles.textoPessoa}>{props.data.cidade}</Text>
+            <Text style={styles.textoPessoa}>{props.data.data}</Text>
         </View>
     )
 }
